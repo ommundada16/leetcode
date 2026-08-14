459786d66c3baf53b50697b4541b9b76abe212d1
ommundada16 <149699106+ommundada16@users.noreply.github.com>
2026-08-14 22:09:57 +0530

    Refactor reverseWords method implementation
    Refactor the reverseWords method to manually split and reverse words.

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,4 +1,42 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         
-        return " ".join(s.split()[::-1])
+        word = ""
+        lst = []
+
+        n = len(s)
+
+        for i in range(n):
+
+            if s[i] != " ":
+                word += s[i]
+
+            else:
+
+                if len(word)!=0:
+                    lst.append(word)
+                    word = "" 
+
+        if len(word)!=0:
+            lst.append(word)
+
+        # n2 = len(lst)
+        # res =""
+        # for i in range(n2-1,-1,-1):
+        #     if i != 0:
+        #         res += lst[i]
+        #         res += " "
+        #     else:
+        #         res += lst[i]
+
+        lst.reverse()
+
+
+
+    
+
+        return " ".join(lst)
+
+
+
+        
