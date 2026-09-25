013ff2c01d029ee05719835aa3d708180a4eff98
ommundada16 <149699106+ommundada16@users.noreply.github.com>
2026-09-25 14:52:44 +0530

    Time: 24 ms (9.07%), Space: 55.5 MB (63.14%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n= nums.length;
+
+        HashMap <Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+
+            if(map.get(nums[i]) > n/2){
+                return nums[i];
+            }
+
+
+        }
+        return -1;
+    }
+}
